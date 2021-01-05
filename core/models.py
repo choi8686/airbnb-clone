8f96f8ca31b9d6eea3510ac76805ee3bab9f35e8
choi8686 <this0924@gmail.com>
2021-01-05 10:35:04 +0900

    Room Model part 1

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     """ Meta class등록 후 abstract = True로 하면 db에는 저장되지 않는다 """
 
     class Meta:
